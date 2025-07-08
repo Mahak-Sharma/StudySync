b08832fed31212050ae2f388659cac799a157c84
Aaradhya2005 <146321020+Aaradhya2005@users.noreply.github.com>
2025-07-08 22:51:46 +0530

    Add files via upload

--- groups/meeting/frontend/vite.config.js
+++ groups/meeting/frontend/vite.config.js
@@ -1,10 +1,5 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-    root: ".",
-    build: {
-        outDir: "dist"
-    },
+export default {
     plugins: [react()]
-}); 
+}; 
