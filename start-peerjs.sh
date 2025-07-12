b77e9f605cdb6e686f163753c1c6d4e3300aac8a
AbhishekMamgain7 <abhishekmamgain799@gmail.com>
2025-07-13 01:39:10 +0530

    Move PeerJS server to correct location (groups/meeting/backend) for Render deployment

---
+++ start-peerjs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Starting PeerJS Server..."
+echo ""
+echo "This will start the PeerJS server on port 9000"
+echo "Make sure you have Node.js installed"
+echo ""
+cd groups/meeting/backend
+node peer-server.js 
