cb2a22994b073245bf03e8f17d3300da5e60c3f3
AbhishekMamgain7 <abhishekmamgain799@gmail.com>
2025-07-15 12:38:17 +0530

    Updated friendsBackend.js

--- src/api/friendsBackend.js
+++ src/api/friendsBackend.js
@@ -8,7 +8,7 @@ const serviceAccount = {
     "type": "service_account",
     "project_id": "studysync-3435a",
     "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "private_key": process.env.FIREBASE_PRIVATE_KEY,
     "client_email": "firebase-adminsdk-fbsvc@studysync-3435a.iam.gserviceaccount.com",
     "client_id": "101362409159616884424",
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
