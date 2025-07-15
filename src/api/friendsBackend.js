cb34103af8e693d38aa0d3bf57edf251fdcd3429
AbhishekMamgain7 <abhishekmamgain799@gmail.com>
2025-07-15 12:24:44 +0530

    Updated friendsBackend.js and Deleted serviceAccountKey.json

--- src/api/friendsBackend.js
+++ src/api/friendsBackend.js
@@ -3,7 +3,20 @@ import cors from 'cors';
 import admin from 'firebase-admin';
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
-const serviceAccount = require('./serviceAccountKey.json');
+
+const serviceAccount = {
+    "type": "service_account",
+    "project_id": "studysync-3435a",
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": "firebase-adminsdk-fbsvc@studysync-3435a.iam.gserviceaccount.com",
+    "client_id": "101362409159616884424",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40studysync-3435a.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+};
 
 const app = express();
 const PORT = 5000;
